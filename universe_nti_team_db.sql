f3b657872e3ff1d6578c03d53384124bdade31b3
alinatkachuk <atkachuk2790@gmail.com>
2021-06-07 13:17:47 +0300

    Added Script of Postgres database

---
+++ universe_nti_team_db.sql
@@ -0,0 +1,12 @@
+-- Database: universe_nti_team_db
+
+-- DROP DATABASE universe_nti_team_db;
+
+CREATE DATABASE universe_nti_team_db
+    WITH 
+    OWNER = postgres
+    ENCODING = 'UTF8'
+    LC_COLLATE = 'Russian_Russia.1251'
+    LC_CTYPE = 'Russian_Russia.1251'
+    TABLESPACE = pg_default
+    CONNECTION LIMIT = -1;
